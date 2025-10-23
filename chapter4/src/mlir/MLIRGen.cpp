6b4f1fe45e12b50ccee21adc93e550352766d493
Rupert Smith <rupert@thesett.com>
2025-10-23 15:23:30 +0100

    Added inliner interface to inline all private call sites, and intra procedural shape inference logic.

--- chapter4/src/mlir/MLIRGen.cpp
+++ chapter4/src/mlir/MLIRGen.cpp
@@ -174,6 +174,10 @@ namespace {
                     function.getFunctionType().getInputs(), getType(VarType{})));
             }
 
+            // If this function isn't main, then set the visibility to private.
+            if (funcAST.getProto()->getName() != "main")
+                function.setPrivate();
+
             return function;
         }
 
